e47d2565d9178eb8156cb3f6a58db189213ef8e4
Chaudhry Talha <gameral@live.com>
2020-08-27 09:23:50 +0800

    MIST Digit Tutorial Finished

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LearningDL4J
  Tutorial Codes for learning DL4J library. Here are the step by step tutorial links:
+ <br />
  [Tutorial 1: Deep Learning for Java (DL4J) Getting Started: Tutorial](https://medium.com/datactw/deep-learning-for-java-dl4j-getting-started-tutorial-2259c76c0a7c?source=friends_link&sk=5c4b6577208333dea366dc71c7c5ef46)
--- src/main/java/com/ibjects/ComputerVision/MINST/MinstClassifier.java
+++ src/main/java/com/ibjects/ComputerVision/MINST/MinstClassifier.java
@@ -28,7 +28,7 @@ import java.util.Random;
 
 public class MinstClassifier {
 
-    private static final String RESOURCES_FOLDER_PATH = "/Users/chaudhrytalha/Documents/LearningDL4J/src/main/resources/mnist_png/";
+    private static final String RESOURCES_FOLDER_PATH = "/Users/chaudhrytalha/Documents/LearningDL4J/src/main/resources/mnist_png";
 
     private static final int HEIGHT = 28;
     private static final int WIDTH = 28;
@@ -37,76 +37,97 @@ public class MinstClassifier {
     private static final int N_SAMPLES_TESTING = 10000;
 
     private static final int N_OUTCOMES = 10;
+    private static long t0 = System.currentTimeMillis();
 
     public static void main(String[] args) throws IOException {
 
         BasicConfigurator.configure();
 
-        long t0 = System.currentTimeMillis();
-        DataSetIterator dsi = getDataSetIterator(RESOURCES_FOLDER_PATH + "training", N_SAMPLES_TRAINING);
+        t0 = System.currentTimeMillis();
+        //System.out.print(RESOURCES_FOLDER_PATH + "/training");
+        DataSetIterator dataSetIterator = getDataSetIterator(RESOURCES_FOLDER_PATH + "/training", N_SAMPLES_TRAINING);
 
-        int rndSeed = 123;
+        buildModel(dataSetIterator);
+
+    }
+
+    private static void buildModel(DataSetIterator dsi) throws IOException {
+
+        int rngSeed = 123;
         int nEpochs = 2;
 
-        System.out.printf("Build model...");
+        System.out.printf("Build Model...");
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(rndSeed).updater(new Nesterovs(0.006, 0.9))
+                .seed(rngSeed)
+                .updater(new Nesterovs(0.006, 0.9))
                 .l2(1e-4).list()
-                .layer(new DenseLayer.Builder().nIn(HEIGHT*WIDTH).nOut(1000).activation(Activation.RELU).weightInit(WeightInit.XAVIER).build())
-                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD).nIn(1000)
-                        .nOut(N_OUTCOMES).activation(Activation.SOFTMAX).weightInit(WeightInit.XAVIER).build())
+                .layer(new DenseLayer.Builder()
+                        .nIn(HEIGHT*WIDTH).nOut(1000).activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER).build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nIn(1000).nOut(N_OUTCOMES).activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER).build())
                 .build();
 
         MultiLayerNetwork model = new MultiLayerNetwork(conf);
         model.init();
+        //Print score every 500 interaction
         model.setListeners(new ScoreIterationListener(500));
 
-        for (int i = 0; i < nEpochs; i++) {
-            model.fit(dsi);
-            System.out.println("Completed Epoch: " + i);
-            dsi.reset();
-            DataSetIterator testDsi = getDataSetIterator(RESOURCES_FOLDER_PATH + "testing", N_SAMPLES_TESTING);
-            System.out.println("Evaluate model...");
-            Evaluation evaluation = model.evaluate(testDsi);
-            System.out.println(evaluation.stats());
-            long t1 = System.currentTimeMillis();
-            double t = (double)(t1-t0)/1000.0;
-            System.out.println("\n\nTotal time: "+t+" seconds");
-        }
+        System.out.printf("Train Model...");
+        model.fit(dsi);
+
+        //Evaluation
+        DataSetIterator testDsi = getDataSetIterator(RESOURCES_FOLDER_PATH+"/testing", N_SAMPLES_TESTING);
+        System.out.printf("Evaluating Model...");
+        Evaluation eval = model.evaluate(testDsi);
+        System.out.print(eval.stats());
+
+        long t1 = System.currentTimeMillis();
+        double t = (double)(t1-t0)/1000.0;
+        System.out.print("\n\nTotal time: "+t+" seconds");
     }
 
     private static DataSetIterator getDataSetIterator(String folderPath, int nSamples) throws IOException {
-        File folder = new File(folderPath);
-        File[] digitFolders = folder.listFiles();
-        NativeImageLoader nil = new NativeImageLoader(HEIGHT, WIDTH);
-        ImagePreProcessingScaler scalar = new ImagePreProcessingScaler(0,1);
+        try {
+            File folder = new File(folderPath);
+            File[] digitFolders = folder.listFiles();
 
-        INDArray input = Nd4j.create(new int[]{nSamples, HEIGHT*WIDTH});
-        INDArray output = Nd4j.create(new int[]{nSamples, N_OUTCOMES});
+            NativeImageLoader nativeImageLoader = new NativeImageLoader(HEIGHT, WIDTH); //28x28
+            ImagePreProcessingScaler scaler = new ImagePreProcessingScaler(0,1); //translate image into seq of 0..1 input values
 
-        int n = 0;
-        for (File digitFolder: digitFolders) {
-            int labelDigit = Integer.parseInt(digitFolder.getName());
-            File[] imageFiles = digitFolder.listFiles();
+            INDArray input = Nd4j.create(new int[]{nSamples, HEIGHT*WIDTH});
+            INDArray output = Nd4j.create(new int[]{nSamples, N_OUTCOMES});
 
-            for (File imageFile : imageFiles) {
-                INDArray img = nil.asRowVector(imageFile);
-                scalar.transform(img);
-                input.putRow(n, img);
-                output.put(n, labelDigit, 1.0);
-                n++;
-            }
+            int n = 0;
+            //scan all 0 to 9 digit subfolders
+            for (File digitFolder: digitFolders) {
+                int labelDigit = Integer.parseInt(digitFolder.getName());
+                File[] imageFiles = digitFolder.listFiles();
+
+                for (File imgFile : imageFiles) {
+                    INDArray img = nativeImageLoader.asRowVector(imgFile);
+                    scaler.transform(img);
+                    input.putRow(n, img);
+                    output.put(n, labelDigit, 1.0);
+                    n++;
+                }
+            }//End of For-loop
+
+            //Joining input and output matrices into a dataset
+            DataSet dataSet = new DataSet(input, output);
+            //Convert the dataset into a list
+            List<DataSet> listDataSet = dataSet.asList();
+            //Shuffle content of list randomly
+            Collections.shuffle(listDataSet, new Random(System.currentTimeMillis()));
+            int batchSize = 10;
+
+            //Build and return a dataset iterator
+            DataSetIterator dsi = new ListDataSetIterator<DataSet>(listDataSet, batchSize);
+            return dsi;
+        } catch (Exception e) {
+            System.out.println(e.getLocalizedMessage());
+            return null;
         }
-
-        DataSet dataSet = new DataSet(input, output);
-        List<DataSet> listDataSet = dataSet.asList();
-        Collections.shuffle(listDataSet, new Random(System.currentTimeMillis()));
-        int batchSize = 10;
-
-        DataSetIterator dsi = new ListDataSetIterator<DataSet>(listDataSet, batchSize);
-        return dsi;
-    }
-
-
-
+    } //End of DataIterator Method
 }
